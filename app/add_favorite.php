2f27af8027acadef30788921ff2d7304a542d316
Conner Ward <cjw5db@virginia.edu>
2018-11-15 11:33:49 -0500

    added comments to add_favorite.php

--- app/add_favorite.php
+++ app/add_favorite.php
@@ -1,10 +1,12 @@
 <?php
   session_start();
+  // if not logged in, then send back to previous page
   if (!isset($_SESSION['logged_in']) or $_SESSION['logged_in'] == FALSE or !isset($_SESSION['email'])){
     header('Location: '.$_SERVER['HTTP_REFERER']);
     exit();
   }
 
+  // if not all data sent in get request, then send back to previous page
   if(empty($_GET["id"])){
     header('Location: '.$_SERVER['HTTP_REFERER']);
     exit();
@@ -12,10 +14,13 @@
 
   $id = htmlspecialchars($_GET['id']);
 
+  //get database connection, sets variable $db
   include('get_db_connection.php');
 
+  //do the query on the database to add a favorite
   $result = pg_query_params($db, "UPDATE users SET favorites = array_append(favorites, $1) WHERE email = $2", array($id, $_SESSION['email']));
 
+  //send user back to previous page
   header('Location: '.$_SERVER['HTTP_REFERER']);
   exit();
 
